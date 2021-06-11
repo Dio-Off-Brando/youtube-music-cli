10e369f5ef6e3620e54762eee3928543108ba08c
Dio Off-Brando <somedudz@protonmail.com>
2021-06-11 20:10:01 +0200

    holy shit i just commited a failing build no

--- src/ui_elements.rs
+++ src/ui_elements.rs
@@ -1,18 +1,14 @@
 // use colored::*;
 
 
-pub fn test_func(text: String){
-    println!("\x1B[7m{}", text)
+pub fn highlight_text(text: String){
+    println!("\x1B[7m{}", text);
 }
 
-//pub fn highlight_element(element: str){
-//
-//}
-//
-//
-//
+
 //pub fn array_menu(array: Box<[str]>){
 //    
+//
 //    
 //}
 
